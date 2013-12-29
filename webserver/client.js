d024632b09e0e11fcf02177b1dfb4797e7a7d154
Alex Gamburg <alex.gamburg@gmail.com>
2013-12-28 22:08:14 -0800

    merge

--- webserver/client.js
+++ webserver/client.js
@@ -7,7 +7,26 @@ var socket = null;
 var resultStrings;
 var titleToUrl = null;
 
+// We use locache to store cached results of complete words
+
+function result_strings_from_result_and_set_titleToUrl(results) {
+  titleToUrl = {};
+  resultStrings = _.map(results, function(elem){
+    var title = elem[0];
+    var url = elem[1];  // TODO: use url later
+    titleToUrl[title] = url;
+    // While I am using a _.each below, it always only has one iteration:
+    // there is just one key and value.
+    //anotherResultString.push(title);
+    return title;
+
+    // return elem.title;
+  });
+  return resultStrings;
+}
+
 $(document).ready(function() {
+  locache.flush();
 
     // var colors = ["red", "blue", "green", "yellow", "brown", "black"];
 
@@ -23,35 +42,48 @@ $(document).ready(function() {
 
   var socket = io.connect('http://localhost:8080');
   socket.on('new_search_results', function (data) {
-    console.log("Received search results: ");
+    console.log("Received search results! ");
     // also check data.query to see what the original query was for
-    console.log(data.results);
+    // console.log(data.results);
 
     // make sure results are for the same query
     if (data.query != currentQuery) {
       return;
     }
 
-    console.log("Setting new results.");
-    //var anotherResultString = [];
-    titleToUrl = {};
-    resultStrings = _.map(data.results, function(elem){
-      var title = elem[0];
-      var url = elem[1];  // TODO: use url later
-      titleToUrl[title] = url;
-      // While I am using a _.each below, it always only has one iteration:
-      // there is just one key and value.
-      //anotherResultString.push(title);
-      return title;
+    if (data.complete_results) {
+      // console.log("Complete results: " + JSON.stringify(data));
+      // Set the cache
+      // Split up query into complete and incomplete words
+      /* Begin code that must be moved to a function */
+      var completed_words = data.query.split(" ");
+      var incomplete_word = null;
+      var last_character = data.query.slice(-1);
+      // var completed_words = console.log();
+      if (last_character != " ") {
+        incomplete_word = completed_words.pop();
+      } else {
+        // for some reason, a trailing space is being left in
+        completed_words.pop();
+      }
+      var completed_words_str = completed_words.join(' ');
+      /* End code that must be moved to a function */
 
-      // return elem.title;
-    });
+      console.log("Putting: " + completed_words_str + " in cache.");
+      locache.set(completed_words_str, data.results);
+    }
+
+
+    // console.log("Setting new results.");
+    //var anotherResultString = [];
+    resultStrings = result_strings_from_result_and_set_titleToUrl(data.results);
+
     //
     //console.log("anotherResultString: " + JSON.stringify(anotherResultString));
     if (!resultStrings) {
       resultStrings = [];
     }
-    console.log("resultStrings: " + JSON.stringify(resultStrings));
+    // console.log("resultStrings: " + JSON.stringify(resultStrings));
     resultsCallback(resultStrings);
     // check currentQuery
     // socket.emit('user_query_changed', { my: 'data' });
@@ -69,24 +101,90 @@ $(document).ready(function() {
   // Put Yahoo load inside Jquery??
   YUI().use('autocomplete', 'autocomplete-filters', 'autocomplete-highlighters', function (Y) {
     Y.one('#ac-input').plug(Y.Plugin.AutoComplete, {
+      maxResults: 7,
       resultFilters    : 'subWordMatch',
       resultHighlighter: 'subWordMatch',
       activateFirstItem: true,
       on               : {
         select  :  function(e) {
+<<<<<<< HEAD
           //console.log("e is:" + JSON.stringify(titleToUrl[e.result.raw]));
           ShowResults(e.result.raw, titleToUrl[e.result.raw]); // call to front-end scripts.js
+=======
+          // console.log("e is:" + JSON.stringify(e.result.raw));
+          ShowResults(e.result); // call to front-end scripts.js
+>>>>>>> 7005e6f1895411f99a479761c3e563e65d6d1af9
         }
       },
       source           : function (query, callback) {
-        console.log("Source is being called.");
-        currentQuery = query;
-        resultsCallback = callback;
+        // console.log("Source is being called.");
 
-        // TODO: implement cacheing using one of the following frameworks:
-        // jStorage, lscache, or locache
 
-        socket.emit('user_query_changed', { query: query });
+
+        // Try to get from cache locache.get("key")
+
+        // Split up query into complete and incomplete words
+        var completed_words = query.split(" ");
+        var incomplete_word = null;
+        var last_character = query.slice(-1);
+        // var completed_words = console.log();
+        if (last_character != " ") {
+          incomplete_word = completed_words.pop();
+        } else {
+          // for some reason, a trailing space is being left in
+          completed_words.pop();
+        }
+        var so_far = [];
+        var possible_completed_strings = [];
+        for (var j = 0; j < completed_words.length; j++) {
+          so_far.push(completed_words[j]);
+          possible_completed_strings.push(so_far.join(' '));
+        }
+        // var completed_words_str = completed_words.join(' ');
+        console.log("possible things in cache: " + possible_completed_strings);
+        var cached_val = null;
+        for (var j = 0; j < possible_completed_strings.length; j++) {
+          // lookup possible_completed_strings[j] in cache
+          cached_val = locache.get(possible_completed_strings[j]);
+          if (cached_val) break;
+        }
+
+
+        // check the cache for the results
+        // TODO: need to check to see if each expanding phrase part is in the cache
+        // Exmaple: "the green car was in the garage", check:
+        // "the", "the green", "the green car", "the green car was", ...., "the green car was in the garage"
+
+
+        // var cached_val = locache.get(completed_words_str);
+
+
+        if (cached_val) {
+          console.log("Object in cache!");
+          // console.log("cached_val: " + JSON.stringify(cached_val));
+          resultStrings = result_strings_from_result_and_set_titleToUrl(cached_val);
+          // console.log("resultStrings: " + JSON.stringify(resultStrings));
+
+          //
+          //console.log("anotherResultString: " + JSON.stringify(anotherResultString));
+          if (!resultStrings) {
+            resultStrings = [];
+          }
+          // console.log("resultStrings: " + JSON.stringify(resultStrings));
+          callback(resultStrings);
+          return;
+
+        } else {
+          // Make the actual query
+
+          currentQuery = query;
+          resultsCallback = callback;
+
+          // TODO: implement cacheing using one of the following frameworks:
+          // jStorage, lscache, or locache
+
+          socket.emit('user_query_changed', { query: query });
+        }
 
         /*
         // Wait a little while without blocking execution, then provide results.
